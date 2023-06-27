4cb0a1076b348b3d8bda4c07f7d2e7e6ddcbf16e
Jakob Olechiw <jolechiw@umich.edu>
2023-06-27 17:21:13 -0400

    Finally linked and didnt segfault

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -r ./build/*
-cmake -S . -B ./build -DCMAKE_C_COMPILER=clang-16 -DCMAKE_CXX_COMPILER=clang++-16 -DCMAKE_CXX_FLAGS="-stdlib=libstdc++" 
+cmake -S . -B ./build -DCMAKE_C_COMPILER=clang-16 -DCMAKE_CXX_COMPILER=clang++-16 -DCMAKE_CXX_FLAGS="-stdlib=libstdc++" -DLLVM_LINK_LLVM_DYLIB=true
 cmake --build build/
