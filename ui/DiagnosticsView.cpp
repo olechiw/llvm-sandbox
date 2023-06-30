b0b7836f89b767ea74dc19c8bb9237577fca617c
Jakob Olechiw <jolechiw@umich.edu>
2023-06-29 22:18:49 -0400

    Log diagnostics

---
+++ ui/DiagnosticsView.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jolechiw on 6/29/23.
+//
+
+#include "DiagnosticsView.h"
---
+++ ui/DiagnosticsView.h
@@ -0,0 +1,14 @@
+//
+// Created by jolechiw on 6/29/23.
+//
+
+#ifndef TESTPROJECT_DIAGNOSTICSVIEW_H
+#define TESTPROJECT_DIAGNOSTICSVIEW_H
+
+
+class DiagnosticsView {
+
+};
+
+
+#endif //TESTPROJECT_DIAGNOSTICSVIEW_H
